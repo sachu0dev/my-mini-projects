b0bd033afea74d7a6b4474eb8e9a55b73c9485e6
sushil_kumar <154815993+sachu0dev@users.noreply.github.com>
2024-01-22 20:55:53 +0530

    added description

--- 00-landing-page/app.js
+++ 00-landing-page/app.js
@@ -1,21 +1,21 @@
 const projects = [
   {
-    name: "calculator",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: " DIY Black-Themed Simple Calculator",
+    info: "Kickstart your coding journey with this DIY Black-Themed Calculator project. Using HTML, CSS, and JavaScript, create a sleek and stylish calculator from scratch. Dive into the world of web development and experience the elegance of simplicity in design and functionality. Add, subtract, multiply, and divide with flair while reducing eye strain with the chic black theme. Ready to code in style? Start building your Black-Themed Calculator now!",
     liveLink: "../calculator/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/calculator",
     img: "img-2",
   },
   {
-    name: "calculator",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Grocery Bud - Your Personal Shopping Assistant",
+    info: "Crafted with HTML, CSS, and JavaScript, Grocery Bud is your personal shopping companion. This project utilizes local storage, allowing you to effortlessly add, delete, and edit items on your shopping list. Enjoy a seamless user experience as you take control of your grocery needs. Simplify your shopping routine with Grocery Bud today!",
     liveLink: "../14-grocery-bud/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/14-grocery-bud",
     img: "img14",
   },
   {
-    name: "Rock Paper Scissor",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Rock, Paper, Scissors with Scoreboard",
+    info: "Experience the classic game with a modern twist. Crafted using HTML, CSS, and JavaScript, this project features a persistent scoreboard using local storage. Challenge yourself or enable auto-play for a dynamic experience. Sharpen your coding skills while enjoying the timeless fun of Rock, Paper, Scissors. Dive in and play today!",
     liveLink: "../rock-paper-scissor/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/rock-paper-scissor",
     img: "img-1",
@@ -29,91 +29,91 @@ const projects = [
   },
   {
     name: "Counter",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    info: "Immerse yourself in the basics of DOM manipulation with this Simple Counter Project. Using HTML, CSS, and JavaScript, elevate your coding skills by creating a dynamic counter that allows you to increase and decrease the count effortlessly. Explore the world of web development with this hands-on project, mastering essential DOM techniques. Start your coding journey now with the Simple Counter Project!",
     liveLink: "../02-counter/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/02-counter",
     img: "img2"
   },
   {
-    name: "Reviews",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Interactive Reviews Carousel",
+    info: "Dive into web development with this Reviews Carousel project featuring HTML, CSS, and JavaScript. Create an engaging user experience with options to navigate to the next review manually or enable auto-change for a dynamic showcase. Explore the power of DOM manipulation as you build this interactive carousel. Elevate your coding skills and captivate your audience with the Interactive Reviews Carousel – your gateway to a more immersive web!",
     liveLink: "../03-reviews/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/03-reviews",
     img: "img3"
   },
   {
-    name: "Navbar",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Responsive Navbar Project",
+    info: "Embark on creating a Responsive Navbar using HTML, CSS, and JavaScript. This project focuses on building a navigation bar that adapts seamlessly to various screen sizes. Enhance user experience with a clean design that transforms gracefully on smaller devices. Dive into the fundamentals of responsive web design and elevate your skills with the Responsive Navbar Project. Start coding for a more accessible and user-friendly website now!",
     liveLink: "../04-navbar/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/04-navbar",
     img: "img4"
   },
   {
-    name: "Sidebar",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Responsive Sidebar Project",
+    info: "Immerse yourself in the world of responsive web design with this Responsive Sidebar project using HTML, CSS, and JavaScript. Craft a sleek and adaptable sidebar that seamlessly adjusts to different screen sizes, enhancing user experience on all devices. Explore the fundamentals of creating a responsive layout and gain practical skills for building user-friendly interfaces. Elevate your web development journey with the Responsive Sidebar Project – a key step towards creating dynamic and accessible websites. Start coding now!",
     liveLink: "../05-sidebar/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/05-sidebar",
     img: "img5"
   },
   {
-    name: "Modal",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Modal Project",
+    info: "Explore the simplicity of modal implementation with this project using HTML, CSS, and JavaScript. Create a modal that smoothly overlays your content, providing a modern and user-friendly interaction. Dive into the basics of handling modals and enhance your web development skills. Start coding now to add a touch of elegance and functionality to your projects!",
     liveLink: "../06-modal/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/06-modal",
     img: "img6"
   },
   {
     name: "Question",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    info: "Discover quick answers to common queries with our Frequently Asked Questions (FAQ) section. This user-friendly resource provides concise explanations to address key inquiries about our project. Whether you're a newcomer or a seasoned user, find the information you need efficiently. Explore the FAQ section for a smoother and more informed experience with our project. Ready to get answers? Dive in now!",
     liveLink: "../07-questions/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/07-questions",
     img: "img7"
   },
   {
-    name: "Filter Menu",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Filter Menu Project",
+    info: "Craft a dynamic Filter Menu using HTML, CSS, and JavaScript. Elevate user experience with responsive filtering options. Dive into the basics of creating an interactive menu and enhance your web development skills. Start coding now for a streamlined and user-friendly project!",
     liveLink: "../08-menu/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/08-menu",
     img: "img8"
   },
   {
-    name: "Video and Preloader",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Video with Preloader Project",
+    info: "Immerse your website in dynamic visuals using HTML, CSS, and JavaScript to embed a captivating video. Enhance user experience with a sleek preloader, ensuring smooth loading transitions. Dive into the fundamentals of multimedia integration and elevate your web development skills. Start coding now for a visually engaging project with seamless loading!",
     liveLink: "../09-video/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/09-video",
     img: "img9"
   },
   {
-    name: "scroll",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Smooth Scroll Project",
+    info: "Achieve a seamless user experience with the Smooth Scroll project. Utilize HTML, CSS, and JavaScript to implement a smooth scrolling feature on your website. Elevate your web development skills by mastering the art of creating a polished and user-friendly scrolling interface. Start coding now for a website that effortlessly guides users through content with style and grace!",
     liveLink: "../10-scroll/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/10-scroll",
     img: "img10"
   },
   {
-    name: "tabs",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Tabs Project",
+    info: "Revamp your website's navigation with a Tabs project using HTML, CSS, and JavaScript. Create a sleek and intuitive interface allowing users to effortlessly switch between content sections. Dive into the essentials of tabbed navigation and enhance your web development skills. Start coding now for a modern, organized, and user-friendly browsing experience!",
     liveLink: "../11-tabs/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/11-tabs",
     img: "img11"
   },
   {
-    name: "countdown",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Countdown Timer Project",
+    info: "Add anticipation to your website with a Countdown Timer project using HTML, CSS, and JavaScript. Create a visually appealing timer for upcoming events or product launches. Dive into the world of time manipulation in web development and enhance your skills. Start coding now for a dynamic and engaging countdown on your website!",
     liveLink: "../12-countdown-timer/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/12-countdown-timer",
     img: "img12"
   },
   {
-    name: "birthday countdown",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Birthday Countdown Project",
+    info: "Celebrate special moments with a Birthday Countdown project using HTML, CSS, and JavaScript. Craft a visually delightful countdown timer to mark the days until the next birthday celebration. Dive into the world of time manipulation and elevate your web development skills. Start coding now to add a touch of excitement to your website and make birthdays even more memorable!",
     liveLink: "../birthday-countdown/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/birthday-countdown",
     img: "img-3"
   },
   {
-    name: "lorem ipsum generator",
-    info: "Experience dynamic design with our Color Change Mini Project. Click the button, and watch as the website's background transforms into a burst of vibrant, random colors, creating a visually engaging and interactive experience.",
+    name: "Lorem Ipsum Generator Project",
+    info: "Enhance your web development skills by creating a Lorem Ipsum Generator using HTML, CSS, and JavaScript. Craft a user-friendly tool that generates placeholder text for design mockups or content testing. Dive into the basics of text generation and improve your coding proficiency. Start coding now for a handy Lorem Ipsum tool that streamlines your design and development process!",
     liveLink: "../13-lorem-ipsum/index.html",
     demoLink: "https://github.com/sachu0dev/15-mini-JavaScript-projects/tree/main/13-lorem-ipsum",
     img: "img13"
