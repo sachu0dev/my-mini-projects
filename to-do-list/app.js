3b0b8bfd8151dac9570d09e94ab6aca3455e7165
sushil_kumar <154815993+sachu0dev@users.noreply.github.com>
2024-01-26 15:19:37 +0530

    add code with alot of error

---
+++ to-do-list/app.js
@@ -0,0 +1,181 @@
+// input selections
+const inputText = document.getElementById('input-text');
+const submitBtn = document.querySelector('.submit-btn');
+const inputBox = document.querySelector('.input-box');
+// tasks container
+const listContainer = document.querySelector('.list-container');
+const tasksContainer = document.querySelector('.task-list');
+// clear tasks btn
+const alert = document.querySelector('.alert');
+const clearBtn = document.querySelector('.clear-btn');
+
+// edit option
+let editElement ;
+let editFlag = false;
+let editID = '';
+
+// ****** EVENT LISTENERS **********
+inputBox.addEventListener('submit', addItem);
+
+
+
+//  ****** FUNCTIONS **********
+function addItem(e){
+  e.preventDefault();
+  const value = inputText.value;
+  console.log(value);
+  const id = new Date().getTime().toString();
+  if(value && !editFlag){
+    createListItems(id,value);
+    // display alert
+    displayAlert('Item added','success');
+    // show container
+    listContainer.classList.add('show-container'); 
+    // add to local storage
+    addToLocalStorage(id,value);
+    // set back to default
+    setBackToDefault();
+  } else if(value && editFlag) {
+    editElement.innerHTML = value;
+    displayAlert('Item edited','success');
+    // edit local storage
+    editLocalStorage(editID,value);
+    setBackToDefault();
+  } else {
+    displayAlert('Please enter a value', 'danger');
+  }
+}
+
+
+// display alret
+function displayAlert(text, action){
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  
+  // remove alert after 1 seconds
+  setTimeout(()=>{
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  },1000);
+}
+
+// clear items
+// clear items
+function clearItems(){
+  const items = document.querySelectorAll('.list-item');
+
+  if(items.length > 0){  // Corrected from items.lenght to items.length
+    items.forEach((item)=>{
+      tasksContainer.removeChild(item);
+    });
+  }
+  listContainer.classList.remove('show-container');
+  displayAlert('All items cleared','danger');
+  localStorage.removeItem('list');
+  setBackToDefault();
+}
+
+
+// delete item
+function deleteItem(e){
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  tasksContainer.removeChild(element);
+  if (tasksContainer.children.length === 0) {
+    listContainer.classList.remove('show-container');
+  }
+  displayAlert('Item deleted','danger');
+  setBackToDefault();
+  removeFromLocalStorage(id);
+}
+// edit item
+function editItem(e){
+  const element = e.currentTarget.parentElement.parentElement;
+  // set edit item
+  editElement = e.currentTarget.parentElement.previousElementSibling;
+  // set form value
+  inputText.value = editElement.textContent;
+  editFlag = true;
+  editID = element.dataset.id;
+  submitBtn.textContent = 'update';
+}
+//  set back to default
+function setBackToDefault(){
+  inputText.value = '';
+  editFlag = false;
+  editID = '';
+  submitBtn.textContent = 'submit';
+}
+
+// ****** LOCAL STORAGE **********
+function addToLocalStorage(id,value){
+  const grocery = {id,value};
+  let items = getLocalStorage();
+  items.push(grocery);
+  localStorage.setItem('list',JSON.stringify(items));
+}
+function removeFromLocalStorage(id){
+  let items = getLocalStorage();
+  items = items.filter((item)=>{
+    if(item.id!== id){
+      return item;
+    }
+  });
+  localStorage.setItem('list',JSON.stringify(items));
+}
+function editLocalStorage(id, value) {
+  let items = getLocalStorage();
+  items = items.map((item) => {
+    if (item.id === id) {
+      item.value = value;
+    }
+    return item;
+  });
+  localStorage.setItem('list', JSON.stringify(items));
+}
+function getLocalStorage(){
+  return localStorage.getItem('list')?JSON.parse(localStorage.getItem('list')):[];
+}
+
+// ****** SETUP ITEMS **********
+
+function setupItems(){
+  let items = getLocalStorage();
+  if(items.length > 0){
+    items.forEach((item)=>{
+      createListItems(item.id,item.value);
+    });
+    listContainer.classList.add('show-container');
+  }
+}
+
+function createListItems(id, value) {
+  const element = document.createElement('article');
+  element.classList.add('list-item');
+  const attr = document.createAttribute('data-id');
+  attr.value = id;
+  element.setAttributeNode(attr);
+  element.innerHTML = `<label class="check-container">
+    <input checked="checked" type="checkbox" />
+    <div class="checkmark"></div>
+  </label>
+  <p class="title">${value}</p>
+  <div class="btn-container">
+    <button type="button" class="edit-btn">
+      <span class="material-symbols-outlined"> edit </span>
+    </button>
+    <button type="button" class="delete-btn">
+      <span class="material-symbols-outlined"> close </span>
+    </button>
+  </div>`;
+
+  // edit / delete btn within the created element
+  const deleteBtn = element.querySelector('.delete-btn');
+  const editBtn = element.querySelector('.edit-btn');
+
+  deleteBtn.addEventListener('click', deleteItem);
+  editBtn.addEventListener('click', editItem);
+
+  // append child
+  tasksContainer.appendChild(element);
+}
