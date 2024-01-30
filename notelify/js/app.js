d9215a7c722eae0cfad3c132a4f69fb6087a7e2e
sushil_kumar <154815993+sachu0dev@users.noreply.github.com>
2024-01-30 14:05:36 +0530

    add: save functionality

--- notelify/js/app.js
+++ notelify/js/app.js
@@ -7,18 +7,19 @@ const notesTitleInput = document.querySelector(".notes-title");
 const notesBodyInput = document.querySelector(".notes-body");
 const addNote = document.querySelector(".add-note-btn");
 const saveNoteBtn = document.querySelector(".save-btn");
+const deleteNoteBtn = document.querySelector(".delete-btn");
 
 // event listeners
 window.addEventListener("DOMContentLoaded", showNotes);
-addNote.addEventListener("click", createNewNote);
+// addNote.addEventListener("click", createNewNote);
 saveNoteBtn.addEventListener("click", saveNote);
 // functions
 function showNotes(){
   const notes = NotesAPI.getAllNotes();
-  console.log(notes);
+  notesList.innerHTML = "";
   notes.forEach(element => {
     notesList.innerHTML += `
-    <div class="note-container ${colors[Math.floor(Math.random() * colors.length)]}">
+    <div class="note-container ${colors[Math.floor(Math.random() * colors.length)]} data-id="${element.id}">
       <div class="note-small-title">${element.title}</div>
       <div class="note-small-body">
         ${element.body}
@@ -26,16 +27,29 @@ function showNotes(){
       <div class="note-small-updated">${element.updated}</div>
     </div>
     `;
+    const noteBox = document.querySelector('.note-container');
+    noteBox.addEventListener("click", editNote);
   });
 }
 function saveNote(){
   const note = {
     title: notesTitleInput.value,
     body: notesBodyInput.value,
-  };
+  }
   NotesAPI.saveNote(note);
   notesTitleInput.value = "";
   notesBodyInput.value = "";
+  showNotes();
+}
+
+function editNote(){
+  const savedNotes = notesList.querySelectorAll(".note-container");
+  const id = savedNotes.dataset.id;
+  const note = {
+    id,
+    title: notesTitleInput.value,
+    body: notesBodyInput.value,
+  }
   showNotes();
 }
 
